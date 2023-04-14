41b632ace022b19e7edbac01184fdfdd56d8a645
Michael Conklin <michaelconklin2004@gmail.com>
2023-04-13 20:25:17 -0400

    updateed js and html

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,80 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$( document ).ready(function () {
-  console.log('ready!');
-});
-  var saveButton = $('.saveBtn');
 
-  saveButton[0].addEventListener('click', function() {
-    console.log('saved!')
+
+tasks = [];
+
+var loadTasks = function(){
+    tasks = JSON.parse(localStorage.getItem("tasks"))
+    if(!tasks) {
+        tasks={};
+    } ;
+    printTasks(tasks)
+}
+
+var printTasks = function(){
+    $.each(tasks, function(list, arr){
+
+        var taskP = $("<p>").addClass("description task-item-" + list).text(arr)
+        
+
+        $("#task-item-" + list).replaceWith(taskP);
+    })
+ }
+
+
+    
+var Today = (moment().format("MMMM D, YYYY"))
+    $("#currentDay").text(Today);
+
+var hourAudit =function(){
+    var currentHour = moment().hour() 
+
+    for(var i=8; i<18; i++){
+        var taskArea = $("#task-"+i)  
+        if(currentHour>i){
+            $(taskArea).addClass("past");
+        } else if (currentHour === i){
+            $(taskArea).addClass("present");
+        }else{
+            $(taskArea).addClass("future")
+        }
+    }
+}
+
+$(".taskBin").on("click", "p", function(){
+    var text =$(this)
+      .text()
+      .trim();
+    var textInput =$("<textarea>")
+      .addClass("form-control")
+      .val(text);
+  
+    $(this).replaceWith(textInput);
+     textInput.trigger("focus");
   });
 
+$(".taskBin").on("blur", "textarea", function() {
+    var text = $(this)
+      .val()
+      .trim();
 
+    var taskP = $("<p>")
+      .addClass("taskItem")
+      .text(text);
 
+    $(this).replaceWith(taskP);
+  });    
 
+  $(".saveBtn").on("click", function(){
+      var index = $(".saveBtn").index(this);
+      tasks[index] = $(this).parent().find(".taskItem").text();
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+
+  setInterval(function(){
+      hourAudit();},1000*60*60);
+
+  loadTasks();
+  hourAudit();
 
   
   // TODO: Add a listener for click events on the save button. This code should
